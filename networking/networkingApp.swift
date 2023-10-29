d8945e745344cc15095ee6a0ba2dbe52d6cd56a7
codewithdarkwa <jnrdrkw1@gmail.com>
2023-10-29 12:37:34 +0000

    Initial Commit

---
+++ networking/networkingApp.swift
@@ -0,0 +1,17 @@
+//
+//  networkingApp.swift
+//  networking
+//
+//  Created by Darkwa John on 29/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct networkingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
